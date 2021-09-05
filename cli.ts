4378727f25e10344ed0208d15005d6adbd90197c
ganariya <ganariya2525@gmail.com>
2021-09-05 19:46:15 +0900

    :sparkles: add deps.ts

--- cli.ts
+++ cli.ts
@@ -1,5 +1,4 @@
-import { sleep } from "https://deno.land/x/sleep/mod.ts";
-import { Command } from "https://deno.land/x/cliffy@v0.19.2/command/mod.ts";
+import { sleep, Command } from "./deps.ts";
 import { render } from "./mod.ts";
 
 const { options } = await new Command()
---
+++ deps.ts
@@ -0,0 +1,4 @@
+import { sleep } from "https://deno.land/x/sleep/mod.ts";
+import { Command } from "https://deno.land/x/cliffy@v0.19.2/command/mod.ts";
+
+export { sleep, Command };
