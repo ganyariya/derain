2cfb501d2d872180d4a854deecf02cb44fd43314
ganariya <ganariya2525@gmail.com>
2021-09-05 20:17:19 +0900

    :bug: fix bug of example

--- example/rainded_deno.ts
+++ example/rainded_deno.ts
@@ -1,6 +1,8 @@
 import { render } from "../mod.ts";
+import { sleep } from "../deps.ts";
 
-for await (const content of render(0.05, 0.002)) {
+for await (const content of render(0.005)) {
   console.clear();
   console.log(content);
+  await sleep(0.1);
 }
